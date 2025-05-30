187d01802fa56aadd5f4f3525dc046de0c082281
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-05-30 16:36:29 +0600

    working on todo component

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -3,8 +3,8 @@ import TodoCard from "./TodoCard"
 
 function TodoContainer() {
     return (
-        <div className="mx-auto text-center">
-            <div>
+        <div className="mx-auto text-center ">
+            <div className="flex justify-between items-center p-5">
                 <Button>
                     Add Todo
                 </Button>
@@ -13,12 +13,18 @@ function TodoContainer() {
                 </Button>
             </div>
             <div className="w-full h-full rounded-xl p-4
-            space-y-3">
-                <TodoCard />
-                {/* <div className="text-2xl rounded-md font-bold p-5 flex justify-center items-center">There is no task pending</div> */}
+            space-y-3 bg-[linear-gradient(to_right,_#DC02C3,_#5C53FE)]">
+                <div className="bg-gray-800 p-5 h-full rounded-xl space-y-3 w-full">
+                    <TodoCard />
+                    <TodoCard />
+                    <TodoCard />
+                    <TodoCard />
+                </div>
 
+                {/* <div className="text-2xl rounded-md font-bold p-5 flex justify-center items-center">There is no task pending</div> */}
+                TodoContainer
             </div>
-            TodoContainer</div>
+        </div>
     )
 }
 
