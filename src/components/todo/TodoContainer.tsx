98dae603fcea091e125e18d616846bf0c79ef835
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-05-31 13:15:19 +0600

    working on remove task

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,16 +1,16 @@
-import { Button } from "../ui/button"
+import type { JSX } from "react/jsx-runtime"
+import { useAppSelector } from "../../redux/hook"
 import TodoCard from "./TodoCard"
+import AddTodoModal from "./AddTodoModal"
+import TodoFilter from "./TodoFilter"
 
 function TodoContainer() {
+    const { todos } = useAppSelector((state) => state.todos)
     return (
         <div className="mx-auto text-center ">
             <div className="flex justify-between items-center p-5">
-                <Button>
-                    Add Todo
-                </Button>
-                <Button>
-                    Filter
-                </Button>
+                <AddTodoModal />
+                <TodoFilter />
             </div>
             <div className="w-full h-full rounded-xl p-2
             space-y-3 bg-[linear-gradient(to_right,_#DC02C3,_#5C53FE)]">
@@ -22,9 +22,16 @@ function TodoContainer() {
                         <p>Description</p>
                         <p className="">Actions</p>
                     </div>
-                    <TodoCard />
-                    <TodoCard />
-                    <TodoCard />
+                    {
+                        todos.map((item: JSX.IntrinsicAttributes & { id: string; title: string; description: string; isCompleted?: boolean }) => {
+                            return (
+                                <TodoCard
+                                    key={item.id}
+                                    {...item}
+                                />
+                            )
+                        })
+                    }
                 </div>
 
                 {/* <div className="text-2xl rounded-md font-bold p-5 flex justify-center items-center">There is no task pending</div> */}
